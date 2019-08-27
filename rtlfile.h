6986a570481459f632c87a284088b8098742e5c9
Bertold Van den Bergh <vandenbergh@bertold.org>
2019-08-27 12:54:40 +0200

    Change date

--- rtlfile.h
+++ rtlfile.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Bertold Van den Bergh (vandenbergh@bertold.org)
+ * Copyright (c) 2019, Bertold Van den Bergh (vandenbergh@bertold.org)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
